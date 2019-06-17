721741a03110f8764e45093889995d9105da3e0a
Bebin <bebin@gmail.com>
2019-06-17 13:44:04 +0100

    .

--- Angry Pirds/GameScene.swift
+++ Angry Pirds/GameScene.swift
@@ -13,27 +13,29 @@ class GameScene: SKScene {
     
     let pirdTexture = SKTexture(imageNamed: "pird.png")
     let boxTexture = SKTexture(imageNamed: "crate.png")
+    let ballRadius = 45
     var boxesArr = Array<SKSpriteNode>() // Set will be better?
     var pird = SKSpriteNode()
     var ball = SKShapeNode()
 
     override func didMove(to view: SKView) {
         
-        let pirdSize = 35
+        let pirdSize = 32
         
         let circularPird = SKSpriteNode(texture: pirdTexture)
-        circularPird.physicsBody = SKPhysicsBody(circleOfRadius: max(circularPird.size.width / 2,
-                                                                     circularPird.size.height / 2))
-        let texturedPird = SKSpriteNode(texture: self.pirdTexture)
-        texturedPird.physicsBody = SKPhysicsBody(texture: self.pirdTexture,
-                                                 size: CGSize(width: circularPird.size.width, height: circularPird.size.height))
+        //circularPird.physicsBody = SKPhysicsBody(circleOfRadius: max(circularPird.size.width / 2,
+        //                                                             circularPird.size.height / 2))
         
-        texturedPird.position = CGPoint(x: 0, y: 0)
-        texturedPird.scale(to: CGSize(width: pirdSize, height: pirdSize))
+        
+        //let texturedPird = SKSpriteNode(texture: self.pirdTexture)
+        //texturedPird.physicsBody = SKPhysicsBody(texture: self.pirdTexture,
+        //                                         size: CGSize(width: circularPird.size.width, height: circularPird.size.height))
+        
+        circularPird.scale(to: CGSize(width: pirdSize, height: pirdSize))
         
         let path = CGMutablePath()
         path.addArc(center: CGPoint.zero,
-                    radius: 45,
+                    radius: CGFloat(ballRadius),
                     startAngle: 0,
                     endAngle: CGFloat.pi * 2,
                     clockwise: true)
@@ -43,9 +45,15 @@ class GameScene: SKScene {
         rangeBall.fillColor = .white
         rangeBall.glowWidth = 0.1
         
-        rangeBall.position = CGPoint(x: -245, y: -100)
         
-        self.pird = texturedPird
+        let rangeBallPositionX = -245
+        let rangeBallPositionY = -100
+        
+        rangeBall.position = CGPoint(x: rangeBallPositionX, y: rangeBallPositionY)
+        
+        circularPird.position = CGPoint(x: rangeBallPositionX, y: rangeBallPositionY)
+        
+        self.pird = circularPird
         self.ball = rangeBall
         
         createSceneContents()
@@ -69,9 +77,7 @@ class GameScene: SKScene {
         if !self.pird.contains(pos)
         {
         
-            print(self.pird.position)
-        
-            let newBoxSize = 40
+            let newBoxSize = 37
         
             let circularNewBox = SKSpriteNode(texture: boxTexture)
             circularNewBox.physicsBody = SKPhysicsBody(circleOfRadius: max(circularNewBox.size.width / 2,
@@ -86,7 +92,8 @@ class GameScene: SKScene {
             scene?.addChild(texturedBox)
         } else
         {
-        print("dupa")
+            // drag pird
+            
         }
         
         
@@ -101,15 +108,31 @@ class GameScene: SKScene {
     
     func touchMoved(toPoint pos : CGPoint) {
         
-        //self.pirdTexture.position = pos
+        //let range = SKRange(value: 0, variance: CGFloat(ballRadius))
+        
         // During the touch moved, limit the movement of pird to the circle around him.
+        
+        if self.pird.contains(pos)
+        {
+            // Lock the touch to the pird.
+            // Make pird follow touch pos
+            
+            //self.pird.position = pos
+        }
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+        self.pird.position = self.ball.position
     }
     
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-
+        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
     }
     
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
